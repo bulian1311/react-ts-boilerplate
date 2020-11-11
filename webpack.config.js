8c2ee3fc162e5be62ce877be43e3204f99f8c84c
bulian1311 <bulian1311@gmail.com>
2020-11-11 16:39:41 +0300

    webpack config fix.

--- webpack.config.js
+++ webpack.config.js
@@ -34,31 +34,3 @@ module.exports = {
     port: 3000
   }
 }
-
-// const path = require('path');
-
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//   entry: './src/index.tsx',
-//   resolve: {
-//     extensions: ['.ts', '.tsx', '.js']
-//   },
-//   output: {
-//     path: path.join(__dirname, '/dist'),
-//     filename: 'bundle.min.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         loader: 'ts-loader'
-//       }
-//     ]
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './public/index.html'
-//     })
-//   ]
-// }
